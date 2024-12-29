bc9304e9f9f4e1a516fc5e84985eb372dbe0b529
CelineMali <seveno.celine@gmail.com>
2024-12-29 19:35:29 +0100

    uibuild

--- index.js
+++ index.js
@@ -52,14 +52,29 @@ app.get("/api/persons", (request, response) => {
   Person.find({})
     .then((persons) => {
       if (persons) {
-        response.json(person);
+        response.json(persons);
       } else {
         response.statusMessage = "getAll: Nobody found";
         response.status(404).end();
       }
     })
-    .catch((e) => {
-      console.log("error", e);
+    .catch(function (error) {
+      if (error.response) {
+        // la requête a été faite et le code de réponse du serveur n’est pas dans
+        // la plage 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // la requête a été faite mais aucune réponse n’a été reçue
+        // `error.request` est une instance de XMLHttpRequest dans le navigateur
+        // et une instance de http.ClientRequest avec node.js
+        console.log(error.request);
+      } else {
+        // quelque chose s’est passé lors de la construction de la requête et cela
+        // a provoqué une erreur
+        console.log("Error", error.message);
+      }
       next(e);
     });
 });
